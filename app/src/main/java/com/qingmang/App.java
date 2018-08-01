6fd2d28160d871c35feb21a623906d4631597aef
caomingyu <361786231@qq.com>
2018-08-01 18:35:34 +0800

    add 下拉刷新

--- app/src/main/java/com/qingmang/App.java
+++ app/src/main/java/com/qingmang/App.java
@@ -1,10 +1,20 @@
 package com.qingmang;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.qingmang.baselibrary.utils.AppUtils;
 import com.qingmang.baselibrary.utils.ForegroundCallbacks;
+import com.qingmang.view.headerFooterView.ClassicsFooter;
+import com.qingmang.view.headerFooterView.ClassicsHeader;
 import com.qlzgzg.network.RetrofitServiceManager;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreater;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreater;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+
 
 /**
  * Created by xiejingbao on 2017/9/14.
@@ -26,6 +36,24 @@ public class App extends Application {
         return foregroundCallbacks;
     }
 
+    static {
+        //设置全局的Header构建器
+        SmartRefreshLayout.setDefaultRefreshHeaderCreater(new DefaultRefreshHeaderCreater() {
+            @Override
+            public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
+//                layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
+                return new ClassicsHeader(context);
+            }
+        });
+        //设置全局的Footer构建器
+        SmartRefreshLayout.setDefaultRefreshFooterCreater(new DefaultRefreshFooterCreater() {
+            @Override
+            public RefreshFooter createRefreshFooter(Context context, RefreshLayout layout) {
+                //指定为经典Footer，默认是 BallPulseFooter
+                return new ClassicsFooter(context);
+            }
+        });
+    }
     @Override
     public void onCreate() {
         super.onCreate();
@@ -37,7 +65,7 @@ public class App extends Application {
 //         * 初始化log,获取是否为debug模式
 //          */
         AppUtils.syncIsDebug(getApplicationContext());
-        AppUtils.isDebug();
+//        AppUtils.isDebug();
 
     }
 
---
+++ app/src/main/java/com/qingmang/view/headerFooterView/ClassicsFooter.java
@@ -0,0 +1,136 @@
+package com.qingmang.view.headerFooterView;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.qlzgzg.nettrade.android.R;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshKernel;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.RefreshState;
+import com.scwang.smartrefresh.layout.constant.SpinnerStyle;
+import com.scwang.smartrefresh.layout.util.DensityUtil;
+
+/**
+ * Created by xiejingbao on 2017/8/12.
+ */
+
+public  class ClassicsFooter extends LinearLayout implements RefreshFooter {
+
+    private TextView mHeaderText;//标题文本
+    private ImageView mArrowView;//下拉箭头
+    private ProgressBar mProgressView;//刷新动画视图
+//    private ProgressDrawable mProgressDrawable;//刷新动画
+
+    public ClassicsFooter(Context context) {
+        super(context);
+        initView(context);
+    }
+    public ClassicsFooter(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.initView(context);
+    }
+    public ClassicsFooter(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.initView(context);
+    }
+    private void initView(Context context) {
+        setGravity(Gravity.CENTER);
+        mHeaderText = new TextView(context);
+//        mProgressDrawable = new ProgressDrawable();
+        mArrowView = new ImageView(context);
+        mProgressView = new ProgressBar(context);
+        mProgressView.setIndeterminateDrawable(ContextCompat.getDrawable(getContext(), R.drawable.bga_refresh_loding));
+        mArrowView.setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.refresh_head_arrow));
+//        mArrowView.parserPaths("M20,12l-1.41,-1.41L13,16.17V4h-2v12.17l-5.58,-5.59L4,12l8,8 8,-8z");
+        addView(mProgressView, DensityUtil.dp2px(30), DensityUtil.dp2px(30));
+        addView(mArrowView, DensityUtil.dp2px(27), DensityUtil.dp2px(27));
+        addView(new View(context), DensityUtil.dp2px(10), DensityUtil.dp2px(10));
+        addView(mHeaderText, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        setMinimumHeight(DensityUtil.dp2px(60));
+
+    }
+    @NonNull
+    public View getView() {
+        return this;//真实的视图就是自己，不能返回null
+    }
+    @Override
+    public SpinnerStyle getSpinnerStyle() {
+        return SpinnerStyle.Translate;//指定为平移，不能null
+    }
+    @Override
+    public void onStartAnimator(RefreshLayout layout, int headHeight, int extendHeight) {
+//        anim.start();//开始动画
+    }
+    @Override
+    public int onFinish(RefreshLayout layout, boolean success) {
+//        anim.stop();//停止动画
+//        if (success){
+//            mHeaderText.setText("刷新完成");
+//        } else {
+//            mHeaderText.setText("刷新失败");
+//        }
+        mProgressView.setVisibility(GONE);
+        mArrowView.setVisibility(VISIBLE);
+        return 500;//延迟500毫秒之后再弹回
+    }
+    @Override
+    public boolean isSupportHorizontalDrag() {
+        return false;
+    }
+    @Override
+    public void onStateChanged(RefreshLayout refreshLayout, RefreshState oldState, RefreshState newState) {
+        switch (newState) {
+            case None:
+            case PullToUpLoad:
+                mHeaderText.setText("上拉加载更多");
+                mArrowView.setVisibility(VISIBLE);//显示下拉箭头
+                mProgressView.setVisibility(GONE);//隐藏动画
+                mArrowView.animate().rotation(180);//还原箭头方向
+                break;
+            case Loading:
+                mHeaderText.setText("正在加载");
+                mProgressView.setVisibility(VISIBLE);//显示加载动画
+                mArrowView.setVisibility(GONE);//隐藏箭头
+                mArrowView.animate().rotation(0);
+                break;
+            case ReleaseToLoad:
+                mHeaderText.setText("释放加载");
+                mArrowView.animate().rotation(0);//显示箭头改为朝上
+                break;
+        }
+    }
+    @Override
+    public void onInitialized(RefreshKernel kernel, int height, int extendHeight) {
+    }
+    @Override
+    public void onHorizontalDrag(float percentX, int offsetX, int offsetMax) {
+    }
+
+    @Override
+    public void setPrimaryColors(int... colors){
+    }
+
+    @Override
+    public void onPullingUp(float percent, int offset, int footerHeight, int extendHeight) {
+
+    }
+
+    @Override
+    public void onPullReleasing(float percent, int offset, int footerHeight, int extendHeight) {
+
+    }
+
+    @Override
+    public boolean setLoadmoreFinished(boolean finished) {
+        return false;
+    }
+}
