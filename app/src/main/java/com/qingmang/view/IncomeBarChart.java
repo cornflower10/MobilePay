99e486265a813657e616b62a0d1acd53ba66b747
caomingyu <361786231@qq.com>
2018-09-12 14:13:59 +0800

    add BarChart

---
+++ app/src/main/java/com/qingmang/view/IncomeBarChart.java
@@ -0,0 +1,99 @@
+package com.qingmang.view;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+
+/**
+ * Created by xiejingbao on 2018/9/12.
+ */
+public class IncomeBarChart extends BarChart {
+    private YAxis leftAxis;             //左侧Y轴
+    private YAxis rightAxis;            //右侧Y轴
+    private XAxis xAxis;                //X轴
+    private Legend legend;              //图例
+    private LimitLine limitLine;
+
+    public IncomeBarChart(Context context) {
+        this(context, null);
+    }
+
+    public IncomeBarChart(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public IncomeBarChart(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initBarChart(this);
+    }
+
+    /**
+     * 初始化BarChart图表
+     */
+    private void initBarChart(BarChart barChart) {
+        /***图表设置***/
+        //背景颜色
+        barChart.setBackgroundColor(Color.WHITE);
+        //不显示图表网格
+        barChart.setDrawGridBackground(false);
+        //背景阴影
+        barChart.setDrawBarShadow(false);
+        barChart.setHighlightFullBarEnabled(false);
+        //不显示边框
+        barChart.setDrawBorders(false);
+        //设置动画效果
+        barChart.animateY(1000, Easing.EasingOption.Linear);
+        barChart.animateX(1000, Easing.EasingOption.Linear);
+        Description description = new Description();
+        description.setEnabled(false);
+        barChart.setDescription(description);
+
+        barChart.setDoubleTapToZoomEnabled(false);
+//禁止拖拽
+        barChart.setDragEnabled(false);
+//X轴或Y轴禁止缩放
+        barChart.setScaleXEnabled(false);
+        barChart.setScaleYEnabled(false);
+        barChart.setScaleEnabled(false);
+//禁止所有事件
+//        barChart.setTouchEnabled(false);
+
+
+        /***XY轴的设置***/
+        //X轴设置显示位置在底部
+        xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xAxis.setAxisMinimum(0f);
+        xAxis.setGranularity(1f);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+
+        leftAxis = barChart.getAxisLeft();
+        rightAxis = barChart.getAxisRight();
+        //保证Y轴从0开始，不然会上移一点
+        leftAxis.setAxisMinimum(0f);
+        rightAxis.setAxisMinimum(0f);
+        rightAxis.setEnabled(false);//不显示右侧y轴
+
+        /***折线图例 标签 设置***/
+        legend = barChart.getLegend();
+        legend.setForm(Legend.LegendForm.LINE);
+        legend.setTextSize(11f);
+        //显示位置
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        //是否绘制在图表里面
+        legend.setDrawInside(false);
+    }
+
+
+}
